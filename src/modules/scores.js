81c8db2759a5347c890bc5aa22a8964186473bb9
lenkon <inoulenkon@gmail.com>
2023-02-09 23:55:13 +0530

    Implement API calls for adding scores and retrieving scores

--- src/modules/scores.js
+++ src/modules/scores.js
@@ -1,21 +1,71 @@
-const points = [
-  { name: 'Jack', point: 45 },
-  { name: 'Richard', point: 90 },
-  { name: 'Jim', point: 85 },
-  { name: 'Mick', point: 100 },
-  { name: 'James', point: 80 },
-  { name: 'George', point: 70 },
-  { name: 'Stephen', point: 65 },
-];
+class Scores {
+  constructor() {
+    this.gameURL = 'https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/';
+    this.id = JSON.parse(localStorage.getItem('game-id')) || [];
+    if (this.id.length < 1) {
+      this.getGameId();
+    }
+  }
 
-let scoresHtml = '';
-scoresHtml += '<table class="table table-hover table-sm table-striped"><tbody>';
-scoresHtml += points.reduce((total, current) => {
-  total += `<tr><td>${current.name}: ${current.point}</td></tr>`;
-  return total;
-}, '');
-scoresHtml += '</tbody></table>';
+  parseId = (str) => {
+    const myArray = str.split(' ');
+    return myArray[3];
+  }
 
-const scores = scoresHtml;
+  getGameId = async () => {
+    // if (this.id.length > 0) {
+    //   return;
+    // }
 
-export default scores;
+    await fetch(this.gameURL, {
+      method: 'POST',
+      body: JSON.stringify({ name: 'Leaderboard Service Game' }),
+      headers: {
+        'Content-Type': 'application/json; charset=UTF-8',
+      },
+    })
+      .then((response) => response.json())
+      .then((json) => {
+        const retrievedId = this.parseId(json.result);
+        // if (json.length > 0) {
+        localStorage.setItem('game-id', JSON.stringify(retrievedId));
+        this.id = JSON.parse(localStorage.getItem('game-id')) || [];
+        // }
+      });
+  }
+
+  submitNewScore = async (name, score) => {
+    const scoresURL = `${this.gameURL + this.id}/scores/`;
+    await fetch(scoresURL, {
+      method: 'POST',
+      body: JSON.stringify({ user: name, score }),
+      headers: {
+        'Content-Type': 'application/json; charset=UTF-8',
+      },
+    }).then((response) => response.json())
+      .then(() => {
+        this.retrieveScores();
+      });
+  }
+
+  showScores = async (scores) => {
+    let scoresHtml = '';
+    scoresHtml += '<table class="table table-hover table-sm table-striped"><tbody>';
+    scoresHtml += scores.reduce((total, current) => {
+      total += `<tr><td>${current.user}: ${current.score}</td></tr>`;
+      return total;
+    }, '');
+    scoresHtml += '</tbody></table>';
+    document.getElementById('score-table-container').innerHTML = scoresHtml;
+  };
+
+  retrieveScores = async () => {
+    const scoresURL = `${this.gameURL + this.id}/scores/`;
+    await fetch(scoresURL).then((response) => response.json())
+      .then((json) => {
+        this.showScores(json.result);
+      });
+  }
+}
+
+export default Scores;
